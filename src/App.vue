521160a0c81c1dc951ac991a7e85ad2b82310a9b
HugoPrado <hpifnmg@gmail.com>
2019-06-10 16:40:05 -0300

    fim aula 13

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <span>
+    <header>
+      <nav-bar logo="Social" url="#" cor="green darken-1"></nav-bar>
+    </header>
+
+    <main>
+      <div class="container">
+        <div class="row">
+          <grid-vue tamanho="4">
+
+            <card-menu-vue>
+              <div class="row valign-wrapper">
+                <grid-vue tamanho="4">
+                  <img src="https://materializecss.com/images/yuna.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+                </grid-vue tamanho="8">
+                <grid-vue tamanho="8">
+                  <span class="black-text">
+                    <h4>Maria Silva</h4>
+                    This is a square image. Add the "circle" class to it to make it appear circular.
+                  </span>
+                </grid-vue>
+              </div>
+            </card-menu-vue>
+
+
+          </grid-vue>
+          <div class="col s8">
+            <router-view/>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <footer-vue cor="green darken-1" logo="Social" descricao="teste de descricao" ano="2019">
+      <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+      <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+    </footer-vue>
+
+
+
+  </span>
+</template>
+
+<script>
+import NavBar from '@/components/layouts/NavBar'
+import FooterVue from '@/components/layouts/FooterVue'
+import GridVue from '@/components/layouts/GridVue'
+import CardMenuVue from '@/components/layouts/CardMenuVue'
+export default {
+  name: 'App',
+  components:{
+    NavBar,
+    FooterVue,
+    GridVue,
+    CardMenuVue
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/layouts/FooterVue.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <span>
+      <footer :class="'page-footer ' + cor">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">{{logo || 'Site'}}</h5>
+                <p class="grey-text text-lighten-4">{{descricao || "Sua Descricao"}}</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <slot></slot>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © {{ano || 2018}} Copyright {{logo || 'Site'}}
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+      </span>
+
+</template>
+
+<script>
+export default {
+  name: 'FooterVue',
+  props:['cor','logo','descricao','ano'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
