f29554b247081100e2c08355a056fc9b0f5ba178
HugoPrado <hpifnmg@gmail.com>
2019-06-10 16:31:46 -0300

    Fim Aula 13

---
+++ src/components/layouts/GridVue.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div :class="'col s' + tamanho">
+    <slot></slot>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'GridVue',
+  props:['tamanho'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
